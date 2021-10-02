5861c66b411dfa6020189cbc16de348a1befe616
aridanemartin <aridanemartin@gmail.com>
2021-10-02 12:29:23 +0100

    traduccion es 2

--- pages/centro-de-escalada.js
+++ pages/centro-de-escalada.js
@@ -1,9 +1,13 @@
-
+import useTranslation from 'next-translate/useTranslation'
+import AppLayout from '../components/AppLayout/AppLayout';
 const Centrodeescalada = () => {
+    let { t } = useTranslation();
+
     return ( 
-        <div> 
+        <AppLayout> 
+            <h1>{t('centro-de-escalada:title')}</h1>
             <p>Este es el centro de escalada</p>
-        </div>
+        </AppLayout>
      );
 }
  
