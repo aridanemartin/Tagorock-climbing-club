cd665de0b802cc6dcb772aa8aba47a6f93585f32
aridanemartin <aridanemartin@gmail.com>
2021-10-17 22:36:25 +0100

    anadida portada responsive

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,9 +1,10 @@
-// import Image from 'next/image';
+import Image from 'next/image';
 
 import HeroLogo from '../HeroLogo/HeroLogo';
 
 
 import styles from './Hero.module.css';
+import Completa from '../../public/images/PortadaCompleta.jpg';
 
 const Hero = () => {
     
@@ -11,31 +12,14 @@ const Hero = () => {
     return ( 
         <> 
             <HeroLogo/>  
-            <div className={`${styles.parallaxWrap} ${styles.jsscroll}`}> 
-                    {/* <div id="hola" className={styles.heroBackground}>
-                        <Image 
-                            
-                            src={ParallaxInferior} 
+            <div className={styles.heroWrap}>                
+                    
+                        <Image    
+                            src={Completa} 
                             alt="Tagorock - Mundo Vertical - Hero Banner" 
                             layout="responsive"
-                            width={600}
-                            height={400}
                             quality={50}
-                        />  
-                    </div>
-                
-                    <div className={styles.heroParallax}>
-                        <Image 
-                            
-                            src={ParallaxSuperior} 
-                            alt="Tagorock - Mundo Vertical - Hero Banner" 
-                            layout="responsive"
-                            width={600}
-                            height={400}
-                            quality={50}
-                        />  
-                    </div> 
-                 */}
+                        />                 
             </div>     
         </>
     );
