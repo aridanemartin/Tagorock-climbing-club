8766ab3410d79516430e5d1e16babb3dfe716b62
aridanemartin <aridanemartin@gmail.com>
2021-10-26 13:36:04 +0100

    "nubes"

--- components/HeroLogo/HeroLogo.js
+++ components/HeroLogo/HeroLogo.js
@@ -1,6 +1,8 @@
 import Image from 'next/image';
 import styles from './HeroLogo.module.css';
 import TagorockLogo from '../../public/images/Tagorock-logo-blanco.webp';  
+import Cloud from '../../public/images/cloud.png';
+
 import useTranslation from 'next-translate/useTranslation';
 
 const HeroLogo = () => {
@@ -19,8 +21,29 @@ const HeroLogo = () => {
                     layout="fill"
                     quality={50}
                     />
-                </div>
-                
+                </div> 
+            </div>
+            <div className={styles.cloudWrap}>
+                <div>
+                        <Image 
+                        className={styles.logoCloudWrap}
+                        src={Cloud} 
+                        alt="Tagorock - Mundo Vertical - Hero Banner" 
+                        layout="fill"
+                        quality={50}
+                        />
+                    </div>
+            </div>
+            <div className={styles.cloudWrap2}>
+                <div>
+                        <Image 
+                        className={styles.logoCloudWrap}
+                        src={Cloud} 
+                        alt="Tagorock - Mundo Vertical - Hero Banner" 
+                        layout="fill"
+                        quality={50}
+                        />
+                    </div>
             </div>
             <h1 className={styles.heroTitle}>{t('index:heroBanner.0')}</h1>
             
