af69567033c5093cad9e4be04b4f91378704ef39
aridanemartin <aridanemartin@gmail.com>
2021-11-02 10:10:30 +0000

    deploy failed

--- next.config.js
+++ next.config.js
@@ -2,4 +2,10 @@ const nextTranslate = require('next-translate')
 
 module.exports = {
     ...nextTranslate(),
+    webpack: (config, { isServer }) => {
+        if (!isServer) {
+          config.resolve.fallback.fs = false;
+        }
+        return config;
+    },
 };
