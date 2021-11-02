7dfbda651c36f52a62bcee75c7f65681702ec690
aridanemartin <aridanemartin@gmail.com>
2021-11-02 16:42:01 +0000

    Update club-de-escalada.js

--- pages/club-de-escalada.js
+++ pages/club-de-escalada.js
@@ -11,38 +11,36 @@ const ClubDeEscalada = () => {
 
     const escaladoras = require('../public/images/2escaladoras.jpg');
     const formacionTecnica = require('../public/images/formacionTecnica.jpg');
-    const formacionDeportiva = require('../public/images/escuela.jpg');
+    const escuela = require('../public/images/formacionTecnica2.JPG');
 
     return ( 
         <>
-        <Nav/>  
-        <HeroTemplate
-            image={escaladoras}
-            alt="Tagorock - Escuela Infantil de Escalada"
-            title1={t('club-de-escalada:heroTitle1-a')}
-            title2={t('club-de-escalada:heroTitle1-b')}
-        />
-        <SocialBanner/>
-        <SectionTemplate
-            image={formacionDeportiva}
-            title1={t('club-de-escalada:escuelaDeEscaladaTitle1')}
-            title2={t('club-de-escalada:escuelaDeEscaladaTitle1')}
-            text1={t('club-de-escalada:escuelaDeEscalada1')}
-            text2={t('club-de-escalada:escuelaDeEscalada2')}
-            url="http://www.google.es"
-            buttonText="Tarifas"
-
-        />
-        <SectionTemplateInverted
-            image={formacionTecnica}
-            title1={t('club-de-escalada:formacionTecnicaTitle1')}
-            title2={t('club-de-escalada:formacionTecnicaTitle2')}
-            text1={t('club-de-escalada:formacionTecnica1')}
-            text2={t('club-de-escalada:formacionTecnica2')}
-            url="http://www.google.es"
-            buttonText="Tarifas"
-        />  
-        
+            <Nav/>  
+            <HeroTemplate
+                image={escaladoras}
+                alt="Tagorock - Escuela Infantil de Escalada"
+                title1={t('club-de-escalada:heroTitle1-a')}
+                title2={t('club-de-escalada:heroTitle1-b')}
+            />
+            <SocialBanner/>
+            <SectionTemplate
+                image={escuela}
+                title1={t('club-de-escalada:escuelaDeEscaladaTitle1')}
+                title2={t('club-de-escalada:escuelaDeEscaladaTitle1')}
+                text1={t('club-de-escalada:escuelaDeEscalada1')}
+                text2={t('club-de-escalada:escuelaDeEscalada2')}
+                url="http://www.google.es"
+                buttonText="Tarifas"
+            />
+            <SectionTemplateInverted
+                image={formacionTecnica}
+                title1={t('club-de-escalada:formacionTecnicaTitle1')}
+                title2={t('club-de-escalada:formacionTecnicaTitle2')}
+                text1={t('club-de-escalada:formacionTecnica1')}
+                text2={t('club-de-escalada:formacionTecnica2')}
+                url="http://www.google.es"
+                buttonText="Tarifas"
+            />     
         </>
      );
 
