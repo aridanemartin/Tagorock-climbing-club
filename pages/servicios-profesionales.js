14d501ecfc8284861a4ab1ec859057452749d053
Aridane Martín <aridanemartin@gmail.com>
2021-11-06 13:30:16 +0000

    test imagenes/traducciones en servicios profesionales

--- pages/servicios-profesionales.js
+++ pages/servicios-profesionales.js
@@ -10,10 +10,13 @@ import Footer from '@components/Footer/Footer';
 const serviciosProfesionales = () => {
     let { t } = useTranslation();
 
+    const portada = require('../public/images/escaleraCentered.jpg');
     const formacionTecnica = require('../public/images/formacionTecnica.jpg');
     const escuela = require('../public/images/escuela.webp');
     const guiaDeMontana = require('../public/images/guiaDeMontana.webp');
-    const portada = require('../public/images/escaleraCentered.jpg');
+    const taladro = require('../public/images/serviciosProfesionales.webp');
+    const rocodromos = require('../public/images/gestionDeRocodromos.jpg');
+    
 
     return ( 
         <>
@@ -28,28 +31,46 @@ const serviciosProfesionales = () => {
             <SectionTemplate
                 id="escuela"
                 image={escuela}
-                title1={t('servicios-profesionales:escuelaDeEscaladaTitle1')}
-                title2={t('servicios-profesionales:escuelaDeEscaladaTitle1')}
-                text1={t('servicios-profesionales:escuelaDeEscalada1')}
-                text2={t('servicios-profesionales:escuelaDeEscalada2')}
+                title1={t('servicios-profesionales:trabajosTecnicosTitle1')}
+                title2={t('servicios-profesionales:trabajosTecnicosTitle2')}
+                text1={t('servicios-profesionales:trabajosTecnicos1')}
+                text2={t('servicios-profesionales:trabajosTecnicos2')}
                 url="http://www.google.es"
                 buttonText="Tarifas"
             />
             <SectionTemplateInverted
                 image={formacionTecnica}
-                title1={t('servicios-profesionales:formacionTecnicaTitle1')}
-                title2={t('servicios-profesionales:formacionTecnicaTitle2')}
-                text1={t('servicios-profesionales:formacionTecnica1')}
-                text2={t('servicios-profesionales:formacionTecnica2')}
+                title1={t('servicios-profesionales:trabajosLocalesTitle1')}
+                title2={t('servicios-profesionales:trabajosLocalesTitle2')}
+                text1={t('servicios-profesionales:trabajosLocales1')}
+                text2={t('servicios-profesionales:trabajosLocales2')}
                 url="http://www.google.es"
                 buttonText="Tarifas"
             />    
             <SectionTemplate
                 image={guiaDeMontana}
-                title1={t('servicios-profesionales:escuelaDeEscaladaTitle1')}
-                title2={t('servicios-profesionales:escuelaDeEscaladaTitle1')}
-                text1={t('servicios-profesionales:escuelaDeEscalada1')}
-                text2={t('servicios-profesionales:escuelaDeEscalada2')}
+                title1={t('servicios-profesionales:arqueologiaVerticalTitle1')}
+                title2={t('servicios-profesionales:arqueologiaVerticalTitle2')}
+                text1={t('servicios-profesionales:arqueologiaVertical1')}
+                text2={t('servicios-profesionales:arqueologiaVertical2')}
+                url="http://www.google.es"
+                buttonText="Tarifas"
+            />
+            <SectionTemplateInverted
+                image={taladro}
+                title1={t('servicios-profesionales:equipamientoDeportivoTitle1')}
+                title2={t('servicios-profesionales:equipamientoDeportivoTitle2')}
+                text1={t('servicios-profesionales:equipamientoDeportivo1')}
+                text2={t('servicios-profesionales:equipamientoDeportivo2')}
+                url="http://www.google.es"
+                buttonText="Tarifas"
+            />    
+            <SectionTemplate
+                image={rocodromos}
+                title1={t('servicios-profesionales:gestionDeRocodromosTitle1')}
+                title2={t('servicios-profesionales:gestionDeRocodromosTitle2')}
+                text1={t('servicios-profesionales:gestionDeRocodromos1')}
+                text2={t('servicios-profesionales:gestionDeRocodromos2')}
                 url="http://www.google.es"
                 buttonText="Tarifas"
             />
